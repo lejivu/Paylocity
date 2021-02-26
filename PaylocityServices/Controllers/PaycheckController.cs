b6f2d4064e5ea80c6fc388e3608e020cc71a1ac4
Jimmy Le <lejivu@gmail.com>
2021-02-25 22:38:05 -0600

    Paylocity Initial

---
+++ PaylocityServices/Controllers/PaycheckController.cs
@@ -0,0 +1,23 @@
+﻿using DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaylocityServices.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PaycheckController : ControllerBase
+    {
+        [Route("{selectionID}"), HttpGet]
+        [Produces(typeof(Paycheck))]
+        public async Task<IActionResult> Get()
+        {
+
+            return Ok();
+        }
+    }
+}
